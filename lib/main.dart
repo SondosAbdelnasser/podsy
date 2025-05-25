60df1a0c0121cf3d6489a9eaba8157b28b5b82b9
sondos-abdelnasser <132229525+SondosAbdelnasser@users.noreply.github.com>
2025-05-26 01:50:35 +0300

    ??

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Podsy',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme,
       home: AuthForm(),
       routes: {
